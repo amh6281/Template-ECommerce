5e6eb6e84656f7092a1124d9aebe2eff0b40b2ba
amh6281 <amh6281@naver.com>
2022-03-31 13:26:55 +0900

    add: Category 컴포넌트 추가 2022.03.31
    data.js의 카테고리별 id, img, title을 Categories 컴포넌트에서 CategoryItem 컴포넌트로 전달한다.

--- src/data.js
+++ src/data.js
@@ -21,3 +21,24 @@ export const sliderItems = [
     bg: "fbf0f4",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1603251579431-8041402bdeda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNoaXJ0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "SHIRT STYLE!",
+    cat: "top",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amFja2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "JACKET STYLE!",
+    cat: "jacket",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1622434641406-a158123450f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdhdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "WATCH",
+    cat: "accessory",
+  },
+];
