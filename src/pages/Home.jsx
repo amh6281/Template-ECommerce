86f5ae99911cf74a937aa0d96c37269feae35ba6
GilTaeWook <xodnr1566@naver.com>
2022-03-31 14:06:05 +0900

    add:Footer 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "../components/Slider";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Announcement />
       <Navbar />
       <Slider />
+      <Footer />
     </div>
   );
 };
