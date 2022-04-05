336eed967e40fc659f089df67913b0709550802a
amh6281 <amh6281@naver.com>
2022-04-05 15:34:19 +0900

    add: Cart 페이지 추가 2022.04.05
    장바구니를 담당하는 Cart 페이지를 추가한다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Home />;
+  return <Cart />;
 }
 
 export default App;
