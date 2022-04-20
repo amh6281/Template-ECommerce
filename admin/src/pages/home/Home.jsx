5a36b1e765215e5b0df38ffb93ba268a7edd16a3
GilTaeWook <xodnr1566@naver.com>
2022-04-20 12:31:42 +0900

    add: 관리자페이지 Chart,widgetLg,widgetSm,User,UserList 페이지 추가 2022.04.20

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from "../../components/chart/Chart";
 import Featuredinfo from "../../components/featuredinfo/Featuredinfo";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <Featuredinfo />
+      <Chart />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 };
