46705826a11a61f50e128ba39bbcffd6545c00ac
amh6281 <amh6281@naver.com>
2022-08-12 15:38:24 +0900

    feat: 사업자 회원가입 및 로그인, 사업자 정보 Update 기능 구현 2022.08.12
    사업자의 Schema를 기반으로 회원가입 및 로그인, Update 기능을 구현한다.

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const entrepreneurRoute = require("./routes/entrepreneur");
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/entrepreneurs", entrepreneurRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Entrepreneur = require("../models/Entrepreneur");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-//REGISTER
+//USER REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -17,6 +18,26 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//ENTREPRENEUR REGISTER
+router.post("/eregister", async (req, res) => {
+  const newEntrepreneur = new Entrepreneur({
+    businessnumber: req.body.businessnumber,
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedEUser = await newEntrepreneur.save();
+    res.status(201).json(savedEUser);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -46,6 +67,39 @@ router.post("/login", async (req, res) => {
     );
 
     const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//ENTREPRENEUR
+router.post("/elogin", async (req, res) => {
+  try {
+    const entrepreneur = await Entrepreneur.findOne({
+      username: req.body.username,
+    });
+    !entrepreneur && res.status(401).json("잘못된 권한입니다.");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      entrepreneur.password,
+      process.env.PASS_SEC
+    );
+    const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    Originalpassword !== req.body.password &&
+      res.status(401).json("잘못된 권한입니다.");
+
+    const accessToken = jwt.sign(
+      {
+        id: entrepreneur._id,
+        isAdmin: entrepreneur.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = entrepreneur._doc;
 
     res.status(200).json({ ...others, accessToken });
   } catch (err) {
