15c4e5e6272b031f53f0d9c740a793fb2012e383
amh6281 <amh6281@naver.com>
2022-09-06 19:04:03 +0900

    feat: 쇼핑몰 수익 fetch 2022.09.06
    전체 쇼핑몰 중, 쇼핑몰의 userId가 currentUser의 userId와 같을경우 해당 쇼핑몰의 수익을 가져온다.

--- admin/src/components/featuredinfo/Featuredinfo.jsx
+++ admin/src/components/featuredinfo/Featuredinfo.jsx
@@ -1,15 +1,53 @@
 import "./featuredinfo.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useState, useEffect } from "react";
+import { userRequest } from "../../requestMethods";
+import { useSelector } from "react-redux";
 
 export default function Featuredinfo() {
+  const [income, setIncome] = useState([]);
+  const [shop, setShop] = useState({});
+  const [perc, setPerc] = useState(0);
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const getShop = async () => {
+      try {
+        const res = await userRequest.get(`/shops?userId="${currentUser._id}"`);
+        setShop(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getShop();
+  }, []);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get(`/orders/income/${shop[0]?._id}`);
+        setIncome(res.data);
+        setPerc((res.data[1]?.total * 100) / res.data[0]?.total - 100);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">수익</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">₩ 867,410</span>
+          <span className="featuredMoney">₩ {income[1]?.total}</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownward className="featuredIcon negative" />
+            % {Math.floor(perc)}{" "}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">전월대비 증감률</span>
