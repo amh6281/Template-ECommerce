be813649fed255d50b24340b2b2ca16e61e125b1
amh6281 <amh6281@naver.com>
2022-09-07 19:44:00 +0900

    feat: 상품 추가 페이지 inputs값 가져오기 2022.09.07
    title, desc, price, stock, categories 등을 이름에따라 target.value를 출력한다.

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -2,37 +2,66 @@ import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
-  const [input, setInput] = useState({});
+  const [inputs, setInputs] = useState({});
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState([]);
 
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(","));
+  };
+  console.log(cat);
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input
+            type="file"
+            id="file"
+            onChange={(e) => setFile(e.target.files)[0]}
+          />
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <input
+            name="title"
+            type="text"
+            placeholder="Apple Airpods"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Desc</label>
-          <input type="text" placeholder="description" />
+          <input
+            name="desc"
+            type="text"
+            placeholder="description..."
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="14000" />
+          <input
+            name="price"
+            type="number"
+            placeholder="14000"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Categories</label>
-          <input type="text" placeholder="shirts" />
+          <input type="text" placeholder="man,shirts" onChange={handleCat} />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <select>
+          <select name="inStock" onChange={handleChange}>
             <option value="true">YES</option>
             <option value="false">NO</option>
           </select>
