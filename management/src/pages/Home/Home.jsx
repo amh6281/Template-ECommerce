6f2d3ce49f983967fa250ebff65136dc7291eb2b
amh6281 <amh6281@naver.com>
2022-09-24 15:02:16 +0900

    feat: Widget 컴포넌트 생성 2022.09.24
    사용자, 주문, 수입 등을 담당하는 Widget 컴포넌트를 생성한다.

--- management/src/pages/Home/Home.jsx
+++ management/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Widget from "../../components/Widget/Widget";
 import "./home.scss";
 
 const Home = () => {
@@ -9,7 +10,13 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts"></div>
       </div>
     </div>
   );
