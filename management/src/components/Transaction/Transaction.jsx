39f2e28b0044cd5e19daf111595c4050d6c6f5fb
amh6281 <amh6281@naver.com>
2022-09-26 11:30:38 +0900

    feat: UserList 페이지 생성 2022.09.26
    MUI DataTable을 사용하여 UserList를 생성한다.

--- management/src/components/Transaction/Transaction.jsx
+++ management/src/components/Transaction/Transaction.jsx
@@ -88,7 +88,9 @@ const Transaction = () => {
               </TableCell>
               <TableCell className="tableCell">{row.customer}</TableCell>
               <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">
+                {row.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </TableCell>
               <TableCell className="tableCell">{row.method}</TableCell>
               <TableCell className="tableCell">
                 <span className={`status ${row.status}`}>{row.status}</span>
