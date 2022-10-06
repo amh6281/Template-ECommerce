72cfaed53a1a77a4367124cdadc99a3d728071e3
amh6281 <amh6281@naver.com>
2022-10-06 15:30:36 +0900

    feat: Management 사용자 fetch 2022.10.06
    Management UserList에서 사용자정보를 가져온다.

--- management/src/pages/List/List.jsx
+++ management/src/pages/List/List.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./list.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import Datatable from "../../components/Datatable/Datatable";
-
+import { publicRequest } from "../../requestMethods";
 const List = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await publicRequest.get("/users");
+        setUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        <Datatable />
+        <Datatable users={users} />
       </div>
     </div>
   );
