f469beef492dab289437a4921504b2cf34d78917
amh6281 <amh6281@naver.com>
2022-10-18 18:09:18 +0900

    feat: ShopList페이지에 검색창 구축 2022.10.18
    ShopList페이지에 검색창을 구축하고 엔터키를 누를경우 클릭과 동일한 이벤트를 부여한다.

--- client/src/pages/SearchShop.jsx
+++ client/src/pages/SearchShop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Search } from "@material-ui/icons";
 import { publicRequest } from "../requestMethods";
 import FilterShops from "../components/FilterShops";
 import TopNav from "../components/TopNav";
@@ -14,13 +15,54 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #f4f4f4;
-  margin: 0px 0px 0px;
+  margin: -6px 0px 0px;
   padding: 18px 46px 18px;
+  border-top: 1px solid #ededed;
+`;
+
+const ShopNav = styled.div`
+  background-color: #f4f4f4;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0px;
+`;
+
+const Left = styled.div`
+  padding: 14px 52px;
+  display: flex;
+  align-items: center;
+`;
+
+const ShopCount = styled.span`
+  font-size: 13px;
+  font-weight: ${(props) => (props.type === "number" ? "600" : "500")};
+  color: #222222;
+`;
+
+const Right = styled.div`
+  margin-right: 52px;
+  padding: 6px 35px 6px 12px;
+  width: 195px;
+  height: 30px;
+  border-radius: 50px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  font-size: 12px;
 `;
 
 const SearchShop = () => {
   const [shops, setShops] = useState([]);
   const query = useLocation().search;
+  const navigate = useNavigate();
+  const [q, setQ] = useState("");
 
   useEffect(() => {
     const fetchShops = async () => {
@@ -30,11 +72,35 @@ const SearchShop = () => {
     fetchShops();
   }, [query]);
 
+  const onKeyPress = (e) => {
+    if (e.key == "Enter") {
+      navigate(`/search?q=${q}`);
+    }
+  };
+
   return (
     <>
       <TopNav />
       <MidNav />
       <Banner />
+      <ShopNav>
+        <Left>
+          <ShopCount type="number">{shops.length}</ShopCount>
+          <ShopCount>개 쇼핑몰</ShopCount>
+        </Left>
+        <Right>
+          <Input
+            type="text"
+            placeholder="입점몰 검색"
+            onChange={(e) => setQ(e.target.value)}
+            onKeyPress={onKeyPress}
+          />
+          <Search
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(`/search?q=${q}`)}
+          />
+        </Right>
+      </ShopNav>
       <Container>
         {shops.map((shop) => (
           <FilterShops key={shop._id} shops={shop} />
--- client/src/pages/ShopList.jsx
+++ client/src/pages/ShopList.jsx
@@ -6,9 +6,10 @@ import Shop from "../components/Shop";
 import Footer from "../components/Footer";
 import axios from "axios";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import CatNav from "../components/CatNav";
 import ShopsNav from "../components/ShopsNav";
+import { Search } from "@material-ui/icons";
 
 const Container = styled.div`
   display: flex;
@@ -17,15 +18,16 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f4f4f4;
   margin: -6px 0px 0px;
-  padding: 0px 46px 18px;
+  padding: 18px 46px 18px;
+  border-top: 1px solid #ededed;
 `;
 
 const ShopNav = styled.div`
-  padding: 14px 52px;
+  background-color: #f4f4f4;
   display: flex;
   align-items: center;
-  background-color: #f4f4f4;
-  margin-top: 1px;
+  justify-content: space-between;
+  padding: 10px 0px;
 `;
 
 const ShopCount = styled.span`
@@ -34,9 +36,35 @@ const ShopCount = styled.span`
   color: #222222;
 `;
 
+const Left = styled.div`
+  padding: 14px 52px;
+  display: flex;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  margin-right: 52px;
+  padding: 6px 35px 6px 12px;
+  width: 195px;
+  height: 30px;
+  border-radius: 50px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  font-size: 12px;
+`;
+
 const ShopList = () => {
   const [shops, setShops] = useState([]);
   const [shopCat, setShopCat] = useState(0);
+  const navigate = useNavigate();
+  const [q, setQ] = useState("");
 
   useEffect(() => {
     const fetchShops = async () => {
@@ -48,6 +76,12 @@ const ShopList = () => {
     fetchShops();
   }, []);
 
+  const onKeyPress = (e) => {
+    if (e.key == "Enter") {
+      navigate(`/search?q=${q}`);
+    }
+  };
+
   return (
     <>
       <TopNav />
@@ -55,8 +89,22 @@ const ShopList = () => {
       <Banner />
       <ShopsNav color={shopCat} shopCat={(e) => setShopCat(e)} />
       <ShopNav>
-        <ShopCount type="number">{shops.length}</ShopCount>
-        <ShopCount>개 쇼핑몰</ShopCount>
+        <Left>
+          <ShopCount type="number">{shops.length}</ShopCount>
+          <ShopCount>개 쇼핑몰</ShopCount>
+        </Left>
+        <Right>
+          <Input
+            type="text"
+            placeholder="입점몰 검색"
+            onChange={(e) => setQ(e.target.value)}
+            onKeyPress={onKeyPress}
+          />
+          <Search
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(`/search?q=${q}`)}
+          />
+        </Right>
       </ShopNav>
       <Container>
         <Shop shops={shops} shopCat={shopCat} />
