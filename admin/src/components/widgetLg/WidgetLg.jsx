8daa811205f6ceacd71b560288729102e3e570b5
amh6281 <amh6281@naver.com>
2022-10-31 17:02:12 +0900

    feat: Admin페이지 버튼클릭시 주문의 status 변경 구현 2022.10.31

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { userRequest } from "../../requestMethods";
+import { publicRequest, userRequest } from "../../requestMethods";
 import { format } from "timeago.js";
 import "./widgetLg.css";
 import { useSelector } from "react-redux";
@@ -12,9 +12,26 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 export default function WidgetLg({ transaction }) {
-  const Button = ({ type }) => {
-    return <button className={"widgetLgButton " + type}>{type}</button>;
+  const [orderList, setOrderList] = useState({});
+
+  const handleUpdate = async (id, order) => {
+    console.log(order._id);
+    const orderList = order;
+    try {
+      const res = await publicRequest.put("/orders/" + id, {
+        status:
+          orderList.status === "상품준비중"
+            ? "배송중"
+            : orderList.status === "배송중"
+            ? "배송완료"
+            : alert("배송완료입니다."),
+      });
+      setOrderList(res.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
   return (
     <TableContainer component={Paper} className="table">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -26,11 +43,12 @@ export default function WidgetLg({ transaction }) {
             <TableCell className="tableCell">상품금액</TableCell>
             <TableCell className="tableCell">주소</TableCell>
             <TableCell className="tableCell">상태</TableCell>
+            <TableCell className="tableCell">주문업데이트</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {transaction.map((order) => (
-            <TableRow key={order.id}>
+            <TableRow key={order._id}>
               <TableCell className="tableCell">{order.buyer_name}</TableCell>
               <TableCell className="tableCell">{order.title}</TableCell>
               <TableCell className="tableCell">{order.date}</TableCell>
@@ -39,9 +57,12 @@ export default function WidgetLg({ transaction }) {
               </TableCell>
               <TableCell className="tableCell">{order.buyer_addr}</TableCell>
               <TableCell className="tableCell">
-                <span className={`status ${order.status}`}>
-                  {order.status === "paid" ? "결제완료" : "상품준비중"}
-                </span>
+                <span className={`status ${order.status}`}>{order.status}</span>
+              </TableCell>
+              <TableCell className="tableCell">
+                <button onClick={() => handleUpdate(order._id, order)}>
+                  Next
+                </button>
               </TableCell>
             </TableRow>
           ))}
