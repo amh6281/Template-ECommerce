bccd65e3169ff20128cd5c4111645357b8039e70
amh6281 <amh6281@naver.com>
2022-11-17 14:16:28 +0900

    feat: newList 기능 구현 2022.11.17
    title, products를 선택하여 newList 기능을 구현한다.

--- api/models/List.js
+++ api/models/List.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const ListSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
+    shopId: { type: String, required: true },
     products: { type: Array },
   },
   { timestamps: true }
